b0645a1ac688d6a2ae23484279b912cfe921653a
Esther <helloecho12345@gmail.com>
2021-12-02 12:56:03 +0000

    add interactive_menu and start case selection

--- directory_copy.rb
+++ directory_copy.rb
@@ -1,4 +1,33 @@
-#student_count = 11
+
+def interactive_menu
+  loop do
+  # 1. print the menu and the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+  # 2. read the input and save it as a variable
+    selection = gets.chomp
+  # 3. do what the user has asked
+    case selection
+      when "1"
+        # input the students
+      when "2"
+        # show the students
+      when "9"
+        exit # this will cause the program to terminate
+      else
+        puts "I don't know what you meant, try again"
+    end
+  # 4. repeat from step 1
+  
+  count = 1
+  
+  while count <= 10
+    puts "s"
+    count = count + 1
+  end
+  
+end
 
 def input_students
   puts "Please enter the names of the students"
